4098e674daf2c40023e23d58c9a08db1b472b746
Matt Flowers <mattmflowers28@gmail.com>
2018-10-10 23:53:55 -0400

    Add title bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import TitleBar from './components/TitleBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TitleBar title="List App" />
       </div>
     );
   }
