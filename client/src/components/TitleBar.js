cab3b78eb5814fc272f7a65cccb1a121079f26c2
Matt Flowers <mattmflowers28@gmail.com>
2018-10-11 15:42:22 -0400

    Update title bar

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -4,21 +4,33 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
   root: {
     flexGrow: 1,
   },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
 };
 
-function HeaderBar(props) {
+function TitleBar(props) {
   const { classes } = props;
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.grow} variant="h6" color="inherit">
             {props.title}
           </Typography>
         </Toolbar>
@@ -27,8 +39,8 @@ function HeaderBar(props) {
   );
 }
 
-HeaderBar.propTypes = {
+TitleBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(HeaderBar);
+export default withStyles(styles)(TitleBar);
