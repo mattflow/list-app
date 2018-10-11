8f397c22c3b3b97fa4ae8020cddd11f1c66a4237
Matt Flowers <mattmflowers28@gmail.com>
2018-10-11 15:51:12 -0400

    Add api configurations

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 8080;
+const loggingType = process.env.LOGGING_TYPE || 'dev';
 const clientPath = path.join(__dirname, 'client/build');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan(loggingType));
+app.use(cors());
+app.use(helmet());
 app.use(express.static(clientPath));
 
 app.get('/', (req, res) => {
