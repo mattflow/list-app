0359a9be1492fd5c5cba52d19a1a9ffbfcb65dfb
Matt Flowers <mattmflowers28@gmail.com>
2018-10-15 16:04:20 -0400

    Set up server clusters

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const path = require('path');
+const cluster = require('cluster');
+const cpus = require('os').cpus();
 
 const port = process.env.PORT || 8080;
 const loggingType = process.env.LOGGING_TYPE || 'dev';
@@ -40,6 +42,16 @@ app.get('/api/lists', (req, res) => {
   ]);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+if (process.env.NODE_ENV === 'production' && cluster.isMaster) {
+  cpus.forEach(() => {
+    cluster.fork();
+  });
+
+  cluster.on('exit', worker => {
+    cluster.fork();
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
